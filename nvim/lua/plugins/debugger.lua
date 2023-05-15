6cafa33ae1bec45acc81cf63206b02faae337626
rodrigo <rodrigovbartholo@gmail.com>
2023-05-15 15:49:00 -0300

    fix debuger

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -1,49 +1,52 @@
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"jay-babu/mason-nvim-dap.nvim",
-	},
-	keys = {
 		{
-			"<leader>du",
-			function()
-				require("dapui").toggle({})
+			"rcarriga/nvim-dap-ui",
+			keys = {
+        -- stylua: ignore
+				{"<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI"},
+			},
+			config = function()
+				local dap = require("dap")
+				local dapui = require("dapui")
+
+				dapui.setup()
+
+				dap.listeners.after.event_initialized["dapui_config"] = function()
+					dapui.open()
+				end
+				dap.listeners.before.event_terminated["dapui_config"] = function()
+					dapui.close()
+				end
+				dap.listeners.before.event_exited["dapui_config"] = function()
+					dapui.close()
+				end
 			end,
-			noremap = true,
-			desc = "Dap UI",
+		},
+
+		{
+			"jay-babu/mason-nvim-dap.nvim",
+			config = function()
+				require("mason-nvim-dap").setup({
+					automatic_setup = true,
+					ensure_installed = {
+						"cpptools",
+						"js",
+					},
+					handlers = {},
+				})
+			end,
 		},
 	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-
-		require("mason-nvim-dap").setup({
-			automatic_setup = true,
-			ensure_installed = {
-				"cpptools",
-			},
-			handlers = {},
-		})
-
-		--Basic debugging keymaps
-		vim.keymap.set("n", "<F5>", dap.continue)
-		vim.keymap.set("n", "<F1>", dap.step_into)
-		vim.keymap.set("n", "<F2>", dap.step_over)
-		vim.keymap.set("n", "<F3>", dap.step_out)
-		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
-		vim.keymap.set("n", "<leader>B", function()
-			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-		end)
-
-		dap.listeners.after.event_initialized["dapui_config"] = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated["dapui_config"] = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited["dapui_config"] = function()
-			dapui.close()
-		end
-	end,
+  --stylua: ignore
+	keys = {
+		--Basic debugging keymap
+		{"<F5>", function() require("dap").continue() end, desc = "Continue"},
+		{"<F1>", function() require("dap").step_into() end, desc = "Step Into"},
+		{"<F2>", function() require("dap").step_over() end, desc = "Step Over"},
+		{"<F3>", function() require("dap").step_out() end, desc = "Step Out"},
+		{"<leader>b", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint"},
+		{"<leader>b", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Breakpoint Condition"},
+	},
 }

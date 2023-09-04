b31be4764a29963d2fd96e4ba8959e8debce0028
rodrigo <rodrigovbartholo@gmail.com>
2023-09-04 07:20:13 -0300

    REFACTOR: Change the mapping in the nvim-cmp and replace the opts function to config function

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -2,6 +2,7 @@
 return {
 	"hrsh7th/nvim-cmp",
 	version = false,
+	event = "InsertEnter",
 	dependencies = {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
@@ -13,19 +14,12 @@ return {
 			check_ts = true,
 		} },
 	},
-	opts = function()
+	config = function()
 		local cmp = require("cmp")
-		local luasnip = require("luasnip")
 
-		local has_words_before = function()
-			local unpack = unpack or table.unpack
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
-
-		return {
+		cmp.setup({
 			completion = {
-				completeopt = "menu, menuone,noinsert",
+				completeopt = "menu,menuone,preview,noselect",
 			},
 			snippet = {
 				expand = function(args)
@@ -34,38 +28,19 @@ return {
 			},
 			-- Key mapping
 			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(),
+				["<C-j>"] = cmp.mapping.select_next_item(),
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }), -- accept currently selected item. set `select` to `false` to only confirm explicitly selected items.
-				-- ["<Space>"] = cmp.mapping.confirm({ select = true }), -- accept currently selected item. set `select` to `false` to only confirm explicitly selected items.
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
+				["<CR>"] = cmp.mapping.confirm({ select = false }), -- accept currently selected item. set `select` to `false` to only confirm explicitly selected items.
 			}),
 
 			-- Snippets sources
 			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
+				{ name = "nvim_lsp" },
 				{ name = "nvim_lua" },
 				{ name = "buffer" },
 				{ name = "path" },
@@ -112,6 +87,6 @@ return {
 					{ name = "cmdline" },
 				}),
 			}),
-		}
+		})
 	end,
 }

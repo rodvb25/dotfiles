b4618ca1bd5b042f77a78f32a00f14b91c61039d
rodrigo <rodrigovbartholo@gmail.com>
2023-10-17 10:54:14 -0300

    Refactor conform.nvim config

--- nvim/lua/rodrigo/plugins/conform.lua
+++ nvim/lua/rodrigo/plugins/conform.lua
@@ -1,34 +1,33 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				cpp = { "clang_format" },
-				c = { "clang_format" },
-				xml = { "xmlformat" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			},
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-			require("conform").format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end, { desc = "Format file on range (in visual mode)" })
-	end,
+	keys = {
+		{
+			"<leader>fm",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = { "n", "v" },
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			cpp = { "clang_format" },
+			c = { "clang_format" },
+			xml = { "xmlformat" },
+		},
+		format_on_save = {
+			async = true,
+			lsp_fallback = true,
+		},
+	},
 }
